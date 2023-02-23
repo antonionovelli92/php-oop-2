ac2be61ede68d9277c5664f685a4a6e12c30bb07
antonionovelli92 <anto.novelli@hotmail.it>
2023-02-23 08:47:30 +0100

    .

--- data/index.php
+++ data/index.php
@@ -8,7 +8,6 @@ $cat_category = new Category('Cat', 'fa-solid fa-cat');
 
 $food = new FoodProduct('Scatoletta', 'Cibo in scatola per gatti', 5.50, $cat_category, ['carne', 'additivi'], '2023-11-01');
 $cibo = new FoodProduct('Scatoletta', 'Cibo in scatola per gatti', 5.50, $dog_category, ['carne', 'additivi'], '2023-11-01');
-
 $toy = new ToyProduct('Gommosso', 'Osso di gomma per cagnolini', 5.50, $dog_category, ['gomma', 'plastica'], 'rosso');
 $giocattolo = new ToyProduct('Gommosso', 'Osso di gomma per cagnolini', 5.50, $cat_category, ['gomma', 'plastica'], 'rosso');
 
